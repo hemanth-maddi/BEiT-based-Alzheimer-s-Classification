4529b0ad6b27e88f31761dbfda15abe41fdb95c9
hemanth-maddi <112233813+hemanth-maddi@users.noreply.github.com>
2024-06-30 20:40:40 +0530

    Update AlzheimerBEiTFinal.py

--- AlzheimerBEiTFinal.py
+++ AlzheimerBEiTFinal.py
@@ -15,10 +15,6 @@ from transformers import AutoModelForImageClassification as BeitForImageClassifi
 from collections import Counter
 from sklearn.model_selection import train_test_split
 
-
-# In[10]:
-
-
 # Define transformations
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -26,40 +22,13 @@ transform = transforms.Compose([
 ])
 
 
-# In[12]:
-
-
 # Load the dataset
 full_data = torchvision.datasets.ImageFolder(root='Dataset', transform=transform)
-
-
-# In[14]:
-
-
-full_data
-
-
-# In[5]:
-
-
-len(full_data)
-
-
-# In[15]:
 
 
 # Split the dataset into train, validation, and test sets
 train_data, temp_data = train_test_split(full_data, test_size=0.3)
 val_data, test_data = train_test_split(temp_data, test_size=0.5)
-
-
-# In[17]:
-
-
-len(test_data)
-
-
-# In[22]:
 
 
 # Create data loaders
@@ -68,43 +37,20 @@ val_loader = DataLoader(val_data, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=32, shuffle=True)
 
 
-# In[29]:
-
-
-len(test_loader)
-
-
-# In[21]:
-
-
-full_data.imgs[6396:6399]
-
-
-# In[24]:
-
-
 # Calculate class weights
 class_counts = Counter(img_tuple[1] for img_tuple in full_data.imgs)
 class_weights = {cls: len(full_data) / count for cls, count in class_counts.items()}
 class_weights = [class_weights[i] for i in range(len(class_weights))]
 
 
-# In[25]:
-
-
 # Convert to tensor
 class_weights = torch.tensor(class_weights, dtype=torch.float)
 
-
-# In[ ]:
 
 
 # Move to GPU if available
 # if torch.cuda.is_available():
 class_weights = class_weights.to('cuda')
-
-
-# In[11]:
 
 
 from torch import nn
@@ -117,28 +63,17 @@ num_classes = len(class_weights)
 model.classifier = nn.Linear(model.classifier.in_features, num_classes)
 
 
-# In[12]:
-
-
 # Define loss function with class weights and optimizer
 criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-
-# In[13]:
 
 
 # Move the model to the GPU
 model = model.to('cuda')
 
 
-# In[14]:
-
 
 num_epochs = 200
-
-
-# In[15]:
 
 
 # Training loop
@@ -157,9 +92,6 @@ for epoch in range(num_epochs):
     print('Epochs Done Till Now:', epoch+1)
 
 
-# In[16]:
-
-
 # Validation loop
 model.eval()
 with torch.no_grad():
@@ -173,9 +105,6 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 print('Validation Accuracy: {} %'.format(100 * correct / total))
-
-
-# In[18]:
 
 
 # Testing loop and metrics
@@ -192,9 +121,6 @@ with torch.no_grad():
         all_predictions.extend(predicted)
 
 
-# In[19]:
-
-
 # Move tensors to CPU memory
 all_labels = [label.cpu() for label in all_labels]
 all_predictions = [prediction.cpu() for prediction in all_predictions]
@@ -204,23 +130,11 @@ cm = confusion_matrix(all_labels, all_predictions)
 print('Confusion Matrix:')
 print(cm)
 
-
-# In[26]:
-
-
-len(all_predictions)
-
-
-# In[24]:
-
-
 from sklearn.metrics import classification_report
 
 # Print the classification report
 print(classification_report(all_labels, all_predictions))
 
-
-# In[ ]:
 
 
 torch.save(model.state_dict(), 'alzheimer_model.pth')
